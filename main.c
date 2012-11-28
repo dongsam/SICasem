6ef45f34ef5d72ff0ad1a83eb0b7f3604e09a2ad
dongsam <admin@zxh.co.kr>
2012-11-28 23:23:22 +0900

    struct add

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "sicasem.h"
 int main(int argc,char *argv[]){
-	printf("LAD ia %d",checkInst("LDA"));
+	printf("LAD ia %d \n",checkInst("LDA"));
+	SIC sic;
+	sic.startMemory=1000;
+	printf("%d",sic.startMemory);
 }
--- sicasem.h
+++ sicasem.h
@@ -3,3 +3,21 @@
 #include <string.h>
 
 int instList();
+
+typedef struct{
+	int startMemory;
+	int lineCount;
+	struct line *startLine;
+	struct line *endLine;
+}SIC;
+
+typedef struct{
+	int lineNumber;
+	int memorySize;
+	int memoryLocation;
+	char *label;
+	char *Inst;
+	char *op1;
+	char *op2;
+	struct line *nextLine;
+}Line;
