83d021b2df749ceaeea7d3a9815a72291ca0aad3
dongsam <admin@zxh.co.kr>
2012-11-29 03:55:14 +0900

    struct test complited

--- main.c
+++ main.c
@@ -1,12 +1,129 @@
 #include "sicasem.h"
 int main(int argc,char *argv[]){
+	if ( argc < 2 ) {
+        printf("프로그램 인수로 소스 파일명을 입력하세요\n");
+        return 0;
+    }
+
+    char buffer[512];
+
 	printf("LAD ia %d \n",checkInst("LDA"));
 	SIC sic;
 	sicInit(&sic);
 
-	lineAdd(&sic,"sum","start","2000",NULL);
+	//lineAdd(&sic,"sum","start","2000",NULL);
 
 	printf("%d\n",sic.startMemory);
 	printf("%d\n",sic.lineCount);	
+
+	FILE *source;
+	source = fopen(argv[1],"r");
+
 	
+	char tmpWord[100];
+	char tmpChar;
+	int line=0;
+	int i=0;
+
+/*
+	if(tmpChar = fgetc(source)){
+		if(tmpChar!='\n'){
+			printf("%c",tmpChar);	
+		}
+	}
+*/	
+	
+
+	while( !feof(source)){
+		
+		//while(tmpChar = fgetc(source)){
+		//	if(tmpChar!='\n'){
+				line++;
+				printf("-----------\n");
+				char tmpLabel[10]="";
+				char tmpInst[10]="";
+				char tmpOp1[10]="";
+				char tmpOp2[10]="";
+				i=0;
+				strcpy(tmpWord,"");
+				while(tmpChar = fgetc(source)){
+					if(tmpChar!='\n' && !feof(source) ){
+						if(tmpChar!='\0' && tmpChar!=' ' && tmpChar!='\t'){
+							printf("%c 1\n",tmpChar);
+							tmpWord[i++]=tmpChar;
+						}else{
+							tmpWord[i++]='\0';
+							break;
+						}
+					}else break;
+				}
+				if(checkInst(tmpWord)){
+					strcpy(tmpInst,tmpWord);
+				}else{
+					strcpy(tmpLabel,tmpWord);
+				}
+
+				i=0;
+				strcpy(tmpWord,"");
+				while(tmpChar = fgetc(source)){
+					if(tmpChar!='\n' && !feof(source)){
+						if(tmpChar!='\0' && tmpChar!=' ' && tmpChar!='\t'){
+							printf("%c 2\n",tmpChar);
+							tmpWord[i++]=tmpChar;
+						}else{
+							tmpWord[i++]='\0';
+							break;
+						}
+					}else break;
+				}
+				if(strlen(tmpInst)<1){
+					strcpy(tmpInst,tmpWord);
+				}else{
+					strcpy(tmpOp1,tmpWord);
+				}
+
+				i=0;
+				strcpy(tmpWord,"");
+				while(tmpChar = fgetc(source)){
+					if(tmpChar!='\n' && !feof(source)){
+						if(tmpChar!='\0' && tmpChar!=' ' && tmpChar!='\t'){
+							printf("%c 3\n",tmpChar);
+							tmpWord[i++]=tmpChar;
+						}else{
+							tmpWord[i++]='\0';
+							break;
+						}
+					}else break;
+				}
+				if(strlen(tmpOp1)<1){
+					strcpy(tmpOp1,tmpWord);
+				}else{
+					strcpy(tmpOp2,tmpWord);
+				}
+				
+				printf("label %s  inst %s  op1 %s op2 %s\n",tmpLabel,tmpInst,tmpOp1,tmpOp2);
+				lineAdd(&sic,line,tmpLabel,tmpInst,tmpOp1,tmpOp2);
+
+		//	}
+		//}
+		//tmpChar = fgetc(source);
+
+
+	}
+	sicPrint(&sic);
+	
+
+
+	//char *tmp;
+	//fscanf(source,"%s\n",tmp);
+	//printf("%s",tmp);
+
+
+/*
+	while ( feof(source) != EOF )
+    {
+        if ( fgets(buffer, 512, source) == NULL ) break;  
+        printf("%s", buffer);
+    }
+*/
 }
--- sicasem.h
+++ sicasem.h
@@ -16,10 +16,10 @@ typedef struct Linetype{
 	int lineNumber;
 	int memorySize;
 	int memoryLocation;
-	char *label;
-	char *inst;
-	char *op1;
-	char *op2;
+	char label[20];
+	char inst[20];
+	char op1[20];
+	char op2[20];
 	struct Linetype *nextLine;
 }Line;
 
@@ -30,7 +30,7 @@ void sicInit(SIC *sic){
 	sic->endLine=NULL;
 }
 
-void lineAdd(SIC *sic,char *label,char *inst,char *op1,char *op2){
+void lineAdd(SIC *sic,int line,char *label,char *inst,char *op1,char *op2){
 	Line *nl;
 	nl=(Line *)malloc(sizeof(Line));
 	if(nl==NULL) printf("full memory\n");
@@ -39,16 +39,45 @@ void lineAdd(SIC *sic,char *label,char *inst,char *op1,char *op2){
 		sic->startMemory=atoi(op1);
 		nl->lineNumber=1;
 	}
-	if(sic->endLine!=NULL){
-		sic->endLine->nextLine=nl;
+	if(sic->endLine==NULL){
+		sic->endLine=nl;
 	}
-	sic->endLine=nl;
+	else{
+		sic->endLine->nextLine=nl;
+		sic->endLine=nl;
+	}
+	
 	sic->lineCount++;
-
 	nl->nextLine=NULL;
-	nl->label=label;
-	nl->inst=inst;
-	nl->op1=op1;
-	nl->op2=op2;
+	
+	strcpy(nl->label,label);
+	strcpy(nl->inst,inst);
+	strcpy(nl->op1,op1);
+	strcpy(nl->op2,op2);
 
+	nl->lineNumber=line;
+
+	printf("추가됨  !%s! %s %s %s\n",label,inst,op1,op2);
+
+}
+
+void sicPrint(SIC *sic){
+	printf("출력합니다.\n");
+	if(sic->startLine==NULL){
+		printf("비엇습니다.\n");
+	}
+	Line *l = sic->startLine;
+	while(l!=NULL){
+		printf("%-5d",l->lineNumber);
+		if(strcmp(l->label,"")){
+			printf("%s\t",l->label);
+		}else{
+			printf("\t\t");
+		}
+		printf("%s\t",l->inst);
+		printf("%s\t",l->op1);
+		
+		l = l->nextLine;
+		printf("\n");
+	}
 }
